2e618bbd606486d35a3e6856fe91f2599e2dd2b3
studzw <studzw@gmail.com>
2017-05-26 22:57:21 +0800

    nodejs

---
+++ nodejs/console/console1.js
@@ -0,0 +1,10 @@
+const str = 'test'
+const obj = {a: 'a'}
+console.log('t')
+console.log('%s', str)
+console.log('%d', 123)
+console.log('%j', obj)
+console.error('Error', obj)
+
+// node console1.js 1> info.log
+// node console1.js 2> error.log
